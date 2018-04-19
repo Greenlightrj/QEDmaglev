2967475b9da661af9a76aa9bb95758a417af192c
Rebecca Jordan <rebecca.jordan@students.olin.edu>
2018-04-18 22:46:13 -0400

    first commit

---
+++ hallsensortest/hallsensortest.ino
@@ -0,0 +1,22 @@
+const int hallSensorPin = 0;
+const int coilPin = 10;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(hallSensorPin, INPUT);
+    pinMode(coilPin, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    //int sensorReadout = analogRead(hallSensorPin);
+   // float voltReadout = (sensorReadout*5 - 2.5)/1024;
+   //Serial.println(voltReadout);
+
+    analogWrite(coilPin, 200);
+   
+    delay(100);
+}
